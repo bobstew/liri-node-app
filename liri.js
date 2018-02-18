e368b7b05e3143289158f444ea5789d6fdebd4ba
Robert <robstewjob@gmail.com>
2018-02-17 19:54:47 -0600

    initial files

---
+++ liri.js
@@ -0,0 +1,9 @@
+// module imports
+
+require("dotenv").config();
+var keys = require("./keys.js");
+var request = require("request");
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var fs = require("fs");
+
